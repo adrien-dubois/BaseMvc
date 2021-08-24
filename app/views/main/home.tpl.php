9619bb94e6f03c545ad596af043c4b47b168b209
adrien-dubois <82801620+adrien-dubois@users.noreply.github.com>
2021-08-24 19:54:40 +0200

    Update home.tpl.php

--- app/views/main/home.tpl.php
+++ app/views/main/home.tpl.php
@@ -1,28 +1,2 @@
-    <div id="content">
-        <div id="header">
-            <div id="identite">
-                <div id="logo"></div>
-                <h1>Votre logo</h1>
-                <h2>Votre slogan</h2>
-                <ul>
-                    <li>
-                        <a href="#">Accueil</a>
-                        Lorem ipsum
-                    </li>
-                    <li>
-                        <a href="#">Équipe</a>
-                        Lorem ipsum
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                        Lorem ipsum
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                        Lorem ipsum
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+
 
